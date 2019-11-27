87c6b2f313500636d362a5ba5637eb73d5d591f5
Andrew Grathwohl <andrew@grathwohl.me>
2019-11-26 20:04:00 -0500

    when source URI is an HTTP or HTTPS address create a readable stream for ffprobe

--- source.js
+++ source.js
@@ -1,10 +1,23 @@
 const ffmpeg = require('fluent-ffmpeg')
+const getUri = require('get-uri')
 const path = require('path')
 
 class Source {
   constructor(uri) {
-    this.uri = path.resolve(uri)
-    this.getProperties()
+    if (uri.startsWith('http')) {
+      this.uri = uri
+      getUri(this.uri, (err, rs) => {
+        if (err) { throw new Error(err.stack || err) }
+        ffmpeg(rs).ffprobe((ffErr, data) => {
+          if (err) { throw new Error(ffErr) }
+          this.properties = data
+          this.properties.format.filename = path.basename(uri)
+        })
+      })
+    } else {
+      this.uri = path.resolve(uri)
+      this.getProperties()
+    }
   }
 
   getProperties(opts = {}) {
