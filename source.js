393b7ad7049ac907bd5de805b1f2a0d66f62ab02
Andrew Grathwohl <andrew@grathwohl.me>
2019-12-01 17:09:27 -0500

    refactor(source.js): fix async for Source class

--- source.js
+++ source.js
@@ -6,24 +6,30 @@ class Source {
   constructor(uri) {
     if (uri.startsWith('http')) {
       this.uri = uri
-      getUri(this.uri, (err, rs) => {
-        if (err) { throw new Error(err.stack || err) }
-        ffmpeg(rs).ffprobe((ffErr, data) => {
-          if (err) { throw new Error(ffErr) }
-          this.properties = data
-          this.properties.format.filename = path.basename(uri)
+      return new Promise((resolve, reject) => {
+        getUri(this.uri, (err, rs) => {
+          if (err) { reject(err.stack || err) }
+          ffmpeg(rs).ffprobe((ferr, data) => {
+            if (ferr) { reject(ferr) }
+            this.properties = data
+            this.properties.format.filename = path.basename(uri)
+            resolve(this)
+          })
         })
       })
     } else {
       this.uri = path.resolve(uri)
-      this.getProperties()
+      return this.getProperties()
     }
   }
 
-  getProperties(opts = {}) {
-    ffmpeg(this.uri).ffprobe((err, data) => {
-      this.properties = data
-      return data
+  async getProperties(opts = {}) {
+    return new Promise((resolve, reject) => {
+      ffmpeg(this.uri).ffprobe((err, data) => {
+        if (err) { reject(err) }
+        this.properties = data
+        resolve(this)
+      })
     })
   }
 }
