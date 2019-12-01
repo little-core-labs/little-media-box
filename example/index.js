8fff9ce852aefe287afd48e580a544ff26d30bb4
Andrew Grathwohl <andrew@grathwohl.me>
2019-12-01 17:22:00 -0500

    refactor(example/): Add example code and test video asset

---
+++ example/index.js
@@ -0,0 +1,21 @@
+const Media = require('../index')
+
+async function main() {
+  const src = await new Media.Source('http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4')
+  // const src = await new Media.Source('test.mp4')
+
+  /* Derive tracks from the source */
+  const videoTrack = new Media.Track.Video(src)
+  const mainAudioTrack = new Media.Track.Audio(src)
+  const secAudioTrack = new Media.Track.Audio(src, 2) // fallback audio at track 3
+
+  /* Create a MediaPackage */
+  const pack = new Media.Package([videoTrack, mainAudioTrack, secAudioTrack])
+  console.log(pack)
+  // console.log(JSON.stringify(pack, null, 2))
+  return pack
+}
+
+// Assign a new source based upon the media file URI provided
+
+main()
