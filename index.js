4f484ce3a0a7ea463dcd1f2a92ab40dcb7b57a63
Andrew Grathwohl <andrew@grathwohl.me>
2019-12-02 19:05:27 -0500

    refactor(index.js): handle exit code properly during muxing

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const SubtitleTrack = Track.SubtitleTrack
 const VideoTrack = Track.VideoTrack
 
 const nanoprocess = require('nanoprocess')
+const path = require('path')
 
 class Delivery {
   constructor(sources, opts = {}) {
@@ -32,20 +33,28 @@ class Package {
       throw new Error('Invalid tracks provided')
     }
     this.opts = opts
+
+    this.muxes = []
+    this.demuxes = []
   }
-  async mux() {
+  mux(options = {}) {
     try {
+      const opts = options //copy
+      const outputUrl = opts.outputUrl || 'mux_output.mkv'
+
       const tracks = this.tracks //copy
-      const cmdOpts = ['-o', 'mux_output.mkv', tracks.shift().source.uri]
+      const cmdOpts = ['-o', outputUrl, tracks.shift().source.uri]
       cmdOpts.push(
         ...tracks.filter(t => t.source.uri !== cmdOpts[2]).map(m => `+${m}`)
       )
-      console.log(cmdOpts)
       const muxCmd = nanoprocess('mkvmerge', cmdOpts)
 
       muxCmd.open((err) => {
-        muxCmd.process.on('close', (exitErr) => {
-          console.log(exitErr, 'closed mux command')
+        muxCmd.process.on('close', (exitCode) => {
+          console.log('closed mux command with code', exitCode)
+          if (exitCode == 0) {
+            this.muxes.push(outputUrl)
+          }
         })
       })
     } catch (e) {
