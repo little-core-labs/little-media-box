39875352b8b4f599cc91b5d18fc9ddb65e0bf854
Andrew Grathwohl <andrew@grathwohl.me>
2019-12-06 21:02:22 -0500

    refactor(target.js): When creating a new Target with a provided Package, ensure the Package object also updates its Targets array

--- target.js
+++ target.js
@@ -9,6 +9,7 @@ class Target {
         throw new Error('Package is not a valid MediaPackage')
       } else {
         this.package = options.package
+        this.package.assignTargets(this)
       }
     }
     if (!options.target) {
@@ -20,6 +21,11 @@ class Target {
       throw new Error('Not a defined target')
     }
     this.config = JSON.parse(fs.readFileSync(`./targets/${options.target}.json`))
+  }
+  assessTasks() {
+    if (!this.package) {
+      console.error('Assign a Package to this Target first')
+    }
   }
 }
 
