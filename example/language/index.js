87a59c3b2ea521ae0de2b73ff10d4e874e79bdb0
Joseph Werle <joseph.werle@gmail.com>
2019-12-19 13:49:55 -0500

    refactor(): Clean up subtitle example

--- example/language/index.js
+++ example/language/index.js
@@ -1,17 +1,25 @@
+const { SubtitleTrack } = require('../../track')
 const { Source } = require('../../source')
+const Batch = require('batch')
 const path = require('path')
 
 const romanceLanguages = [ 'fre', 'spa', 'por' ]
 const source = new Source(path.resolve(__dirname, 'example.mkv'))
 
 source.probe((err, probe) => {
-  const subtitleStreams = []
+  const tracks = []
+  const batch = new Batch()
 
   for (const stream of probe.streams) {
     if ('subtitle' === stream.codec_type) {
-      subtitleStreams.push(stream)
+      const track = SubtitleTrack.from(source.uri, { streamIndex: stream.index })
+      tracks.push(track)
+      batch.push((next) => track.ready(next))
     }
   }
 
-  console.log(subtitleStreams)
+  batch.end((err) => {
+    if (err) { throw err }
+    console.log(tracks.map((track) => track.language));
+  })
 })
