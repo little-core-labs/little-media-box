f9aeb9025cb396ca01ddb0826c5acf74e742e778
Andrew Grathwohl <andrew@grathwohl.me>
2020-01-21 22:09:49 -0500

    refactor(): Finalized file extensions example script

--- example/file-extensions/index.js
+++ example/file-extensions/index.js
@@ -1,4 +1,5 @@
-const lmb = require('../../.')
+const fs = require('fs')
+const { Source, extensions } = require('../../.')
 const path = require('path')
 
 const sourceUris = [
@@ -11,26 +12,22 @@ const sourceUris = [
 console.log('None of these dang files have the right extensions!', sourceUris)
 
 for (const u of sourceUris) {
-    const src = new lmb.Source(u)
+    const src = new Source(u)
     src.open((err) => {
         function onprobe(err, data) {
             let fileFormat = data.format.format_name
-            const theType = lmb.extensions.typeof(fileFormat)
+            const theType = extensions.typeof(fileFormat)
             console.log(`\nCorrect file basename for ${src.uri} is:`)
             if (theType === 'unknown') {
                 fileFormat =  fileFormat.split(',')[0]
-                console.log(`${src.uri}.${fileFormat === 'matroska' ? 'mkv' : fileFormat}`)
-            } else {
-                console.log(`${src.uri}.${fileFormat}`)
+                fileFormat = fileFormat === 'matroska' ? 'mkv' : fileFormat
             }
+
+            const newFile = `${u}.${fileFormat}`
+            fs.renameSync(u, newFile)
+            console.log(newFile)
         }
+
         src.probe(onprobe)
     })
 }
-/*
-
-const src = new lmb.Source('./src1')
-console.log(src)
-
-*/
-
